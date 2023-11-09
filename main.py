ead397a14f799ae596722a93206c66aca525febe
Herman Dylan <74062720+HermanDylan@users.noreply.github.com>
2023-11-09 22:57:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import random
 import math
- 
+
 # A set will be the collection of prime numbers,
 # where we can select random primes p and q
 prime = set()
- 
+
 public_key = None
 private_key = None
 n = None
- 
+
 # We will run the function only once to fill the set of
 # prime numbers
 def primefiller():
@@ -20,13 +20,13 @@ def primefiller():
     for i in range(2, 250):
         for j in range(i * 2, 250, i):
             seive[j] = False
- 
+
     # Filling the prime numbers
     for i in range(len(seive)):
         if seive[i]:
             prime.add(i)
- 
- 
+
+
 # Picking a random prime number and erasing that prime
 # number from list because p!=q
 def pickrandomprime():
@@ -35,38 +35,38 @@ def pickrandomprime():
     it = iter(prime)
     for _ in range(k):
         next(it)
- 
+
     ret = next(it)
     prime.remove(ret)
     return ret
- 
- 
+
+
 def setkeys():
     global public_key, private_key, n
-    prime1 = pickrandomprime()  # First prime number
-    prime2 = pickrandomprime()  # Second prime number
- 
-    n = prime1 * prime2
-    fi = (prime1 - 1) * (prime2 - 1)
- 
+    p = pickrandomprime()  # First prime number
+    q = pickrandomprime()  # Second prime number
+
+    n = p * q
+    z = (p - 1) * (q - 1)
+
     e = 2
     while True:
-        if math.gcd(e, fi) == 1:
+        if math.gcd(e, z) == 1:
             break
         e += 1
- 
+
     # d = (k*Φ(n) + 1) / e for some integer k
     public_key = e
- 
+
     d = 2
     while True:
-        if (d * e) % fi == 1:
+        if (d * e) % z == 1:
             break
         d += 1
- 
+
     private_key = d
- 
- 
+
+
 # To encrypt the given number
 def encrypt(message):
     global public_key, n
@@ -77,8 +77,8 @@ def encrypt(message):
         encrypted_text %= n
         e -= 1
     return encrypted_text
- 
- 
+
+
 # To decrypt the given number
 def decrypt(encrypted_text):
     global private_key, n
@@ -89,8 +89,8 @@ def decrypt(encrypted_text):
         decrypted %= n
         d -= 1
     return decrypted
- 
- 
+
+
 # First converting each character to its ASCII value and
 # then encoding it then decoding the number to get the
 # ASCII and converting it to character
@@ -100,25 +100,25 @@ def encoder(message):
     for letter in message:
         encoded.append(encrypt(ord(letter)))
     return encoded
- 
- 
+
+
 def decoder(encoded):
     s = ''
     # Calling the decrypting function decoding function
     for num in encoded:
         s += chr(decrypt(num))
     return s
- 
- 
+
+
 if __name__ == '__main__':
     primefiller()
     setkeys()
-    message = "Test Message"
+    #message = "Test Message"
     # Uncomment below for manual input
-    # message = input("Enter the message\n")
+    message = input("Enter the message\n")
     # Calling the encoding function
     coded = encoder(message)
- 
+
     print("Initial message:")
     print(message)
     print("\n\nThe encoded message(encrypted by public key)\n")
